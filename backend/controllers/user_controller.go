f0a7fa4ef09e12f5ef46b9f6cfa5fd0121983ec9
soyr_6o <soyrozero@gmail.com>
2026-05-23 10:32:35 +0330

    feat(backend:add user login)

--- backend/controllers/user_controller.go
+++ backend/controllers/user_controller.go
@@ -60,3 +60,38 @@ func Register(c *fiber.Ctx) error {
 
 	return c.Status(fiber.StatusCreated).JSON(fiber.Map{"message":"success create user"})
 }
+
+func Login(c *fiber.Ctx) error {
+	var data map[string]string
+	ctx,cancel := context.WithTimeout(context.Background(),5 * time.Second)
+	defer cancel()
+
+	if err := c.BodyParser(&data); err != nil{
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error":"Invalid request body"})
+	}
+
+	var user models.User
+
+	if err := databases.DB.WithContext(ctx).Where("email = ?",data["email"]).First(&user).Error; err != nil{
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error":"Filed find user"})
+	}
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password),[]byte(data["password"])); err!= nil {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error":"incorrect password"})
+	}
+
+	token,err := utils.GenerateJwt(user.ID)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error":"Failed generate token"})
+	}
+
+	cookie := fiber.Cookie{
+		Name: "jwt",
+		Value: token,
+		Expires: time.Now().Add(24 * time.Hour),
+		HTTPOnly: true,
+	}
+
+	c.Cookie(&cookie)
+
+	return c.JSON(fiber.Map{"message":"login success"})
+}
