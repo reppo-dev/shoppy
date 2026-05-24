8696666138f4a20eef38bb7df06f487610439c67
soyr_6o <soyrozero@gmail.com>
2026-05-24 11:18:36 +0330

    fix(backend):update routes

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -10,4 +10,10 @@ func Setup(app *fiber.App) {
 	app.Post("/register",controllers.Register)
 	app.Post("/login",controllers.Login)
 	app.Post("/logout",controllers.Logout)
+
+	app.Get("/allproducts",controllers.AllProducts)
+	app.Get("/getproduct",controllers.GetProduct)
+	app.Post("/createproduct",controllers.CreateProduct)
+	app.Put("/updateproduct",controllers.UpdateProduct)
+	app.Delete("/deleteproduct",controllers.DeleteProduct)
 }
