87114d42d357ba8bd92862058d6cb08e56c8b993
soyr_6o <soyrozero@gmail.com>
2026-05-28 12:05:30 +0330

    feat(backend):add delete category controller

--- backend/controllers/category.go
+++ backend/controllers/category.go
@@ -106,3 +106,27 @@ func UpdateCategory(c *fiber.Ctx) error {
 	return c.JSON(fiber.Map{"message":"success update category"})
 
 }
+
+func DeleteCategory(c *fiber.Ctx) error {
+	ctx,cancel := context.WithTimeout(context.Background(),5*time.Second)
+	defer cancel()
+
+	id , err := strconv.Atoi(c.Params("id"))
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error":"Invalid request id"})
+	}
+
+	var category models.Category
+
+	if err := databases.DB.WithContext(ctx).First(&category,id).Error; err!= nil{
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error":"Failed found category"})
+	}
+
+	if err := databases.DB.WithContext(ctx).Delete(&category).Error; err !=nil{
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error":"Failed delete category"})
+	}
+
+	return c.JSON(fiber.Map{
+		"message":"success delete catefory",
+	})
+}
