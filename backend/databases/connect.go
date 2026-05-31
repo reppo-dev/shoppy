35cac284cc96cebaf49610603cffcca54d67b69e
mohammad amin <soyrozero@gmail.com>
2026-05-31 08:55:19 +0330

    Update connect.go

--- backend/databases/connect.go
+++ backend/databases/connect.go
@@ -11,8 +11,10 @@ var DB *gorm.DB
 
 func Connect() {
 
+	// for example ⭣⭣⭣
 	dns := "host=localhost user=postgres password=13802002 dbname=shoppy port=5432 sslmode=disable"
-
+	// dns := "host=your_host user=your_databace password=your_password_databace dbname=name_db port=port_database sslmode=disable"
+	
 	db, err := gorm.Open(postgres.Open(dns),&gorm.Config{})
 	if err !=nil {
 		panic("we can't connect database")
